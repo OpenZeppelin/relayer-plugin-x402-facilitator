af55903892cf4fbb01b2458069ae1a61849b8b03
Collins <delanhype@gmail.com>
2026-02-19 18:13:19 +0300

    fix: Fixing build (#24)

--- scripts/release.js
+++ scripts/release.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { exec, getExecOutput } = require("@actions/exec");
 
 const { version } = require("../package.json");
-const tag = `${version}`;
+const tag = `v${version}`;
 
 process.chdir(path.join(__dirname, ".."));
 
@@ -26,5 +26,7 @@ process.chdir(path.join(__dirname, ".."));
 
   await exec("changeset", ["publish"]);
 
-  await exec("changeset", ["tag"]);
+  // Create and push the tag explicitly (Changesets defaults to v-prefixed tags).
+  await exec("git", ["tag", tag]);
+  await exec("git", ["push", "origin", tag]);
 })();
